89d27a5148d5e379f8f294660bb7a067bea344eb
Izzat_Melikov <gagarin.yurick@mail.ru>
2026-07-30 00:00:55 +0300

    fix: fix intro component for main page

--- components/mainIntro/intro.tsx
+++ components/mainIntro/intro.tsx
@@ -1,10 +1,55 @@
+'use client';
+
+import Image from "next/image";
 import styles from "./intro.module.css";
+
+const TEAM_AVATARS = [
+  { name: 'Закирова Элина', img: '/Team/Закирова Элина.png' },
+  { name: 'Гадецкая Елизавета', img: '/Team/Гадецкая Елизавета.png' },
+  { name: 'Гадецкая Диана', img: '/Team/Гадецкая Диана.png' },
+  { name: 'Герасимова Дарья', img: '/Team/Герасимова Дарья.png' },
+  { name: 'Фролова Елизавета', img: '/Team/Фролова Елизавета.png' },
+  { name: 'Попова Дарина', img: '/Team/Попова Дарина.png' },
+  { name: 'Худяк Дарья', img: '' },
+  { name: 'Хмара Елизавета', img: '/Team/Хмара Елизавета.png' },
+];
 
 export default function Intro() {
   return (
     <section className={styles.section}>
       <div className={styles.grid}>
-        <div className={styles.eyebrow}>Наша студия</div>
+      
+        <div className={styles.leftCol}>
+          <div className={styles.eyebrow}>Наша студия</div>
+          
+          <div className={styles.avatarArea}>
+            {TEAM_AVATARS.map((member, index) => (
+              <div 
+                key={member.name} 
+                className={`${styles.miniAvatar} ${styles[`pos${index + 1}`]}`}
+                style={{ zIndex: index + 1 }}
+              >
+                {member.img ? (
+                  <Image 
+                    src={member.img} 
+                    alt={member.name} 
+                    width={64} 
+                    height={64} 
+                    className={styles.avatarImg}
+                  />
+                ) : (
+                  <span className={styles.initials}>
+                    {member.name
+                      .split(' ')
+                      .map((word) => word[0])
+                      .join('')}
+                  </span>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+
         <div>
           <p className={styles.description}>
             Проектируем спортивные комплексы и коммерческие объекты любого масштаба. 
@@ -12,6 +57,7 @@ export default function Intro() {
             Реализуем проекты под ключ: от первой концепции до финальной сдачи объекта.
           </p>
         </div>
+
       </div>
     </section>
   );
