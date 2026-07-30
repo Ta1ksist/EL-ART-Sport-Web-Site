3cf63c850fe1a3913beb5f4119bc41780318b87c
Izzat_Melikov <gagarin.yurick@mail.ru>
2026-07-30 14:49:52 +0300

    fix: new project

--- lib/projects.ts
+++ lib/projects.ts
@@ -14,15 +14,72 @@ export interface Project {
 }
 
 export const PROJECTS: Project[] = [
-  {
-    slug: "pash-padel-soon",
-    cover: "/projects/soon/pash-padel-soon.png",
-    title: "Падел-комплекс «Pash padel»",
-    type: "Падел-комплекс",
+  // {
+  //   slug: "pash-padel-soon",
+  //   cover: "/projects/soon/pash-padel-soon.png",
+  //   title: "Падел-комплекс «Pash padel»",
+  //   type: "Падел-комплекс",
+  //   area: "3500 м²",
+  //   location: "Россия, Москва",
+  //   year: "В разработке",
+  //   isSoon: true
+  // },
+  { 
+    slug: "place-padel-astr", 
+    cover: "/projects/preview/preview-place-padel-astr.jpg", 
+    title: "Падел-комплекс «Place padel»", 
+    type: "Падел-комплекс", 
+    location: "Россия, Астрахань", 
+    year: 2026,
+    designCode: "Кодовые слова: энергия, динамика, движение, функциональность, структура.",
+    designPrinciple: "Открытая экосистема — создавая первый падел-клуб в Астрахани, мы сделали ставку на абсолютную прозрачность. Игровые корты и зоны отдыха бесшовно соединены. Это позволяет гостям легко считывать правила игры и проникаться атмосферой корта еще до того, как они возьмут в руки ракетку. \nЛогика гостеприимства — интерьер спроектирован так, чтобы убрать любое стеснение у новичков. Маршрут от ресепшена, открытой зоны примерки мерча и лаунжа интуитивно понятен. Мы ушли от концепции закрытых спортивных залов в сторону дружелюбного и комфортного пространства для жизни и общения.",
+    area: "1800 м²",
+    timeline: "3 месяца",
+    gallery: [
+      "/projects/place-padel-astr/1.jpg", 
+      "/projects/place-padel-astr/2.jpg", 
+      "/projects/place-padel-astr/3.jpg",
+      "/projects/place-padel-astr/4.jpg",
+      "/projects/place-padel-astr/5.jpg",
+    ]
+  },
+  { 
+    slug: "pash-padel-msc", 
+    cover: "/projects/preview/preview-pash-padel-msc.jpg", 
+    title: "Падел-комплекс «Pash padel»", 
+    type: "Падел-комплекс", 
+    location: "Россия, Москва", 
+    year: 2026,
+    designCode: "Кодовые слова: комфорт, общение, тепло, расслабление, непринужденность.",
+    designPrinciple: "Статус — это то, что считывается с первой секунды. Мы создали пространство, которое мгновенно заявляет о высоком уровне клуба. Сочетание глубоких терракотовых оттенков, геометрии света и текстуры бетона формирует премиальный образ объекта и отстраивает бизнес от обычных игровых залов. \nСценарий гостя — мы продумали каждую точку контакта. Движение посетителей, акустика в зоне ожидания, правильный свет, который не слепит и создает уют — все это складывается в единый бесшовный опыт, превращающий случайного игрока в постоянного резидента клуба.",
     area: "3500 м²",
-    location: "Россия, Москва",
-    year: "В разработке",
-    isSoon: true
+    timeline: "3 месяца",
+    gallery: [
+      "/projects/pash-padel-msc/1.jpg", 
+      "/projects/pash-padel-msc/2.jpg", 
+      "/projects/pash-padel-msc/3.jpg",
+      "/projects/pash-padel-msc/4.jpg",
+      "/projects/pash-padel-msc/5.jpg",
+    ]
+  },
+  { 
+    slug: "studio-flex-msc", 
+    cover: "/projects/preview/preview-studio-flex-msc.jpg", 
+    title: "Студия женского фитнеса «THE FLEX»", 
+    type: "Фитнес", 
+    location: "Россия, Москва", 
+    year: 2026,
+    designCode: "Пространство, где спорт становится частью образа жизни: тренировки, основанные на женской физиологии, сообщество, в котором нет давления, пространство, где хочется оставаться.",
+    designPrinciple: "Пространство было разработано многоуровневым, часть функций перенесены на второй этаж: тренерские, технические узлы и комната отдыха для персонала.",
+    area: "450 м²",
+    timeline: "3 месяца",
+    gallery: [
+      "/projects/studio-flex-msc/1.jpg", 
+      "/projects/studio-flex-msc/2.jpg", 
+      "/projects/studio-flex-msc/3.jpg", 
+      "/projects/studio-flex-msc/4.jpg",
+      "/projects/studio-flex-msc/5.jpg"
+    ]
   },
   { 
     slug: "rocket-padel-kemerovo", 
@@ -61,25 +118,6 @@ export const PROJECTS: Project[] = [
       "/projects/rocket-padel-vlg/3.png",
       "/projects/rocket-padel-vlg/4.png",
       "/projects/rocket-padel-vlg/5.png",
-    ]
-  },
-  { 
-    slug: "studio-flex-msc", 
-    cover: "/projects/preview/preview-studio-flex-msc.jpg", 
-    title: "Студия женского фитнеса «THE FLEX»", 
-    type: "Фитнес", 
-    location: "Россия, Москва", 
-    year: 2025,
-    designCode: "Пространство, где спорт становится частью образа жизни: тренировки, основанные на женской физиологии, сообщество, в котором нет давления, пространство, где хочется оставаться.",
-    designPrinciple: "Пространство было разработано многоуровневым, часть функций перенесены на второй этаж: тренерские, технические узлы и комната отдыха для персонала.",
-    area: "450 м²",
-    timeline: "3 месяца",
-    gallery: [
-      "/projects/studio-flex-msc/1.jpg", 
-      "/projects/studio-flex-msc/2.jpg", 
-      "/projects/studio-flex-msc/3.jpg", 
-      "/projects/studio-flex-msc/4.jpg",
-      "/projects/studio-flex-msc/5.jpg"
     ]
   },
 ];
